72c1dc8745b8b14d881df0fdf678bb7c850c1afa
Nick <nickgomvelez@gmail.com>
2024-10-18 09:29:38 -0500

    fix: fix category model

--- models/category.js
+++ models/category.js
@@ -1,18 +1,21 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('./index'); 
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  }, {
+    tableName: 'categories',
+    timestamps: false,
+  });
 
-exports.Category = sequelize.define('Category', {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false
-  }
-}, {
-  tableName: 'categories',  
-  timestamps: false         
-});
+  Category.associate = function(models) {
+    Category.hasMany(models.Item, {
+      foreignKey: 'categoryId',
+      as: 'items',
+    });
+  };
 
+  return Category;
+};
