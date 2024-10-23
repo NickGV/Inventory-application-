d7b3e115836b49144eac23aac9c738f65900e9c5
Nick <nickgomvelez@gmail.com>
2024-10-23 09:32:55 -0500

    change variables for the database conection

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ module.exports = {
   development: {
     username: process.env.DB_USER || "default_user",
     password: process.env.DB_PASS || "default_password",
-    database: process.env.DB_NAME || "inventorydb",
+    database: process.env.DB_NAME || "inventory_db_dev",
     host: process.env.DB_HOST || "127.0.0.1",
     port: process.env.DB_PORT || 5432,
     dialect: "postgres",
@@ -12,19 +12,22 @@ module.exports = {
   test: {
     username: process.env.DB_USER || "default_user",
     password: process.env.DB_PASS || "default_password",
-    database: process.env.DB_NAME_TEST || "inventorydb_test",
+    database: process.env.DB_NAME_TEST || "inventory_db_test",
     host: process.env.DB_HOST || "127.0.0.1",
     dialect: "postgres",
   },
   production: {
-    use_env_variable: "DATABASE_URL",
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    host: process.env.PGHOST,
+    port: process.env.PGPORT || 5432,
     dialect: "postgres",
     dialectOptions: {
       ssl: {
         require: true,
-        rejectUnauthorized: false,
+        rejectUnauthorized: false, 
       },
     },
-    logging: false,
   },
 };
